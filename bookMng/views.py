e84dc369aab8a29ed24c0bee29c158d590cd487f
AirTenTony <64555962+AirTenTony@users.noreply.github.com>
2021-11-18 00:02:58 -0800

    Merge branch 'main' of https://github.com/YoussefE95/BookManager

--- bookMng/views.py
+++ bookMng/views.py
@@ -124,6 +124,7 @@ def book_delete(request, book_id):
     return render(request, 'bookMng/book_delete.html')
 
 
+
 @login_required(login_url=reverse_lazy('login'))
 def faq(request):
     books = Book.objects.all()
@@ -136,3 +137,6 @@ def faq(request):
                       'books': books,
                   })
 
+
+def shopping_cart(request):
+    return render(request, 'bookMng/shopping_cart.html')
